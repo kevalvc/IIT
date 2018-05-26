d3011c9a0284d20f5fef00c020bcc4a4dda4fdec
kevalvc <chavda.keval97@gmail.com>
2018-05-26 13:04:58 +0530

    Fixed load popup for initial page

--- js/graphFunctions.js
+++ js/graphFunctions.js
@@ -30,8 +30,8 @@ var index = 0;
 // request.send();
 // importXML();
 // });
-
-if (localStorage.getItem("localXMLVal") == "") {
+console.log("aadwe "+localStorage.getItem("localXMLVal"));
+if (localStorage.getItem("localXMLVal") == "" || localStorage.getItem("localXMLVal") == null) {
   // alert("aa");
   retr();
 }
@@ -49,7 +49,20 @@ function redoChange() {
 function retr() {
   var retrievedGraph = ($(".hidden-xml-op").val());
   if (retrievedGraph == "") {
-
+    if (localStorage.getItem("localXMLVal") == "") {
+      xml = '<mxGraphModel><root><mxCell id="0"/><mxCell id="1" parent="0"/></root></mxGraphModel>';
+      console.log("XML"+xml);
+      toastr.error('The username and password do not match.', 'Load Failed!');
+      main(document.getElementById('mainer'));
+    } else if (localStorage.getItem("localXMLVal") == null) {
+      xml = '<mxGraphModel><root><mxCell id="0"/><mxCell id="1" parent="0"/></root></mxGraphModel>';
+      console.log("XML"+xml);
+      main(document.getElementById('mainer'));
+    } else {
+      xml = localStorage.getItem("localXMLVal");
+      console.log("XML"+xml);
+      importXML();
+    }
   } else {
     xml = retrievedGraph;
     localStorage.setItem("localXMLVal", xml);
