d491e4c796730cf329dede80629edde7df9668f6
REDswapnil <swapnilarya68@gmail.com>
2018-05-31 23:51:42 +0530

    Minor Changes to Database

--- process.php
+++ process.php
@@ -4,9 +4,30 @@
 $uname = (htmlspecialchars($_POST['username']));
 $filename = (htmlspecialchars($_POST['filename']));
 $contents = (htmlspecialchars($_POST['content']));
+$flag = 1;
 // $content = "<script></script>";
 // echo "$contents";
 
+
+$connect=mysqli_connect('localhost','root','','mydatabase');
+if ($connect === false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$result = mysqli_query($connect,"SELECT * FROM iit") or die (mysql_error());
+$row = mysqli_fetch_array($result);
+// echo $row['uname'];
+while ($row = mysqli_fetch_array($result)) {
+	if($row['uname'] == $uname && $row['filename'] == $filename)
+	{
+	echo "Error : Similiar Filename and Usernames Exists";
+	$flag = 0;
+	break;
+	}
+}
+
+if($flag == 1)
+{
 mysqli_query($connect,"INSERT INTO iit (uname, filename, contents)
 		VALUES ('$uname','$filename','$contents');");
 
@@ -19,7 +40,7 @@ if(mysqli_affected_rows($connect) > 0){
 	echo mysqli_error ($connect);
 	echo "<a href='index.php'>Go Back</a>";
 }
-
+}
 // print_r($_POST);
 
 // echo $_POST['first_name'];
